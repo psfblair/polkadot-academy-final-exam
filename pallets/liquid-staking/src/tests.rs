68f014ddd0bd9416aebb28c19cc5c3d59e9a8fa1
Paul Blair <ec2-user@ip-172-31-11-208.us-east-2.compute.internal>
2022-08-03 10:43:36 +0000

    Fix mut

--- pallets/liquid-staking/src/tests.rs
+++ pallets/liquid-staking/src/tests.rs
@@ -279,7 +279,7 @@ fn redeem_stake_stores_quantity_of_derivative_token_redeemed_along_with_unbondin
 
 		// Assert Account 1 is due 20 dot at the end of the unbonding period:
 		// Mock sets us in Era 2 with a bonding duration of 3, so we expect to get them back in era 5
-		let expected  = BoundedBTreeMap::<EraIndex, BalanceTypeOf<T>, LiquidStakingModule::WithdrawalBound>::new();
+		let mut expected  = BoundedBTreeMap::<EraIndex, BalanceTypeOf<T>, LiquidStakingModule::WithdrawalBound>::new();
 		let _ = expected.try_insert(5, 20);
 		assert_eq!(LiquidStakingModule::redemptions_awaiting_withdrawal(user_account_id), Some(expected) );
 	});
