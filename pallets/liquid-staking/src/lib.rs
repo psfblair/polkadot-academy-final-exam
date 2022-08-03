7e12f2a7ac33ba5d912ddc010359582db5f49865
Paul Blair <ciriwe@phobot.net>
2022-08-03 11:09:03 +0100

    Fix

--- pallets/liquid-staking/src/lib.rs
+++ pallets/liquid-staking/src/lib.rs
@@ -303,7 +303,7 @@ pub mod pallet {
 						let mut new_map = BoundedBTreeMap::<EraIndex, BalanceTypeOf<T>, T::WithdrawalBound>::new();
 						new_map.try_insert(era_available, amount).map(
 							// We know the key isn't actually already there because we just created the map. qed 
-							|_| RedemptionsAwaitingWithdrawal::<T>::insert(who, new_map),
+							|_| RedemptionsAwaitingWithdrawal::<T>::insert(who.clone(), new_map),
 						).map_err(
 							// This Err case should never happen because this is a new map. But if it does it means the bounds have 
 							// been exceeded. Seems like unreasonable work to create a new error for this misconfiguration case.
